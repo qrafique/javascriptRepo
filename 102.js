36fa0a7c90d749668d58252f5bea1c8b0035f826
qasim <qrafique@gmail.com>
2023-10-22 20:10:35 +0500

    7th version

--- 102.js
+++ 102.js
@@ -4,3 +4,6 @@ mainHeading.textContent = "do not manage tasks";
 mainHeading.innerHTML = "hello again changed";
 const h2 = document.querySelector("div.headline h2");
 h2.innerHTML = "once again changed";
+h2.style.color ="blue";
+h2.style.backgroundColor ="white";
+h2.style.border ="6px solid green";
