d272136a965ae5a0735422b886a66ec623bc6985
qasim <qrafique@gmail.com>
2023-10-22 19:53:16 +0500

    6th version

--- 102.js
+++ 102.js
@@ -1,3 +1,6 @@
 //DOM (DOUMENT OBJECT MODEL)
 const mainHeading = document.getElementById("main-heading");
-mainHeading.textContent = "do not manage tasks"
+mainHeading.textContent = "do not manage tasks";
+mainHeading.innerHTML = "hello again changed";
+const h2 = document.querySelector("div.headline h2");
+h2.innerHTML = "once again changed";
