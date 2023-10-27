5053b6a9d8b97aefbb3fa028b2b9035f7ec115d6
qasim <qrafique@gmail.com>
2023-10-27 17:58:57 +0500

    vr 12

--- 111.js
+++ 111.js
@@ -1,3 +1,20 @@
-const rootNode = document.getRootNode();
-console.log(rootNode);
-console.log(rootNode.childNodes);
+// const rootNode = document.getRootNode();
+// console.log(rootNode);
+// const htmlNode = rootNode.childNodes[0];
+// console.log(htmlNode);
+// console.log(htmlNode.childNodes);
+// const headEementNode = htmlNode.childNodes[0];
+// console.log(headEementNode);
+// const textElementNode = htmlNode.childNodes[1];
+// console.log(textElementNode);
+// const bodyElementNode = htmlNode.childNodes[2];
+// console.log(bodyElementNode);
+// console.log(headEementNode.parentElement);
+// console.log(headEementNode.nextSibling.nextSibling);
+// console.log(headEementNode.nextSibling.nextSibling.textContent);
+// console.log(headEementNode.nextSibling.nextSibling.childNodes);
+// console.log(headEementNode.nextSibling.nextSibling.childNodes[3]);
+// headEementNode.nextElementSibling.nextSibling.childNodes[3].textContent= "hello para has been cheanged!"
+const h1 = document.querySelector("h1");
+const h1parent = h1.parentNode;
+h1parent.style.backgroundColor = "#333" 
