e22d1b706b9ea21b015877f073a2d991a7196277
qrafique <82672119+qrafique@users.noreply.github.com>
2023-10-28 11:15:37 +0000

    VR15

--- 112.js
+++ 112.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 const sectionToDo = document.querySelector(".todo-section");
 console.log(sectionToDo.classList);
 //sectionToDo.classList.add("bg-dark")
-=======
 const todosection =document.querySelector("todo-section")
 console.log(todosection);
 todosection.classList.add("todoColor");
->>>>>>> a531e1dd8995a1b96b66db51e1c5dd359c2ddabb
