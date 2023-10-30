fd66a4d8ea160bf11a5cada65e4d89cd7be7cb19
qrafique <82672119+qrafique@users.noreply.github.com>
2023-10-30 04:44:17 +0000

    vr15

--- 112.js
+++ 112.js
@@ -3,4 +3,5 @@ console.log(sectionToDo.classList);
 //sectionToDo.classList.add("bg-dark")
 const todosection =document.querySelector("todo-section")
 console.log(todosection);
-todosection.classList.add("todoColor");
+const mainh2 = document.querySelector(".todo-section")
+mainh2.insertAdjacentHTML("afterend","<section > <div><p>hello this is is sii i </p></div></section>")
