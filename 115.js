23eb1f4c592049bbea4eacfb7e9f41270bd9bc66
qasim <qrafique@gmail.com>
2023-10-30 23:04:02 +0500

    1103P301023

--- 115.js
+++ 115.js
@@ -1,6 +1,20 @@
-const btn = document.querySelector(".btn-headline");
-btn.addEventListener("click", function(){
-    console.log("event clicked btn");
-    console.log(this);
-    
-});
+console.log("hello world");
+const allBtn = document.querySelectorAll(".my-button button");
+console.log(allBtn);
+// allBtn.addEventListener("click", ()=>{
+//     console.log("you clicked btn 1");
+// });
+// for (const button  of allBtn) {
+//    button.addEventListener("click", function () {
+//         console.log(button.textContent);
+//    })
+// }
+// for (let index = 0; index < allBtn.length; index++) {
+//     allBtn[index].addEventListener("click", function (params) {
+//         console.log(this.textContent);
+//     })
+    allBtn.forEach(function(button) {
+        button.addEventListener("click",function() {
+            console.log(this.textContent);
+        })
+    });
