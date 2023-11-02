5d14ef03fada46d4cab85b561db33ac5745166de
qasim <qrafique@gmail.com>
2023-11-02 20:08:58 +0500

    2007110223

--- 115.js
+++ 115.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+console.log("script start .......");
 const allBtn = document.querySelectorAll(".my-button button");
 console.log(allBtn);
 // allBtn.addEventListener("click", ()=>{
@@ -6,15 +6,27 @@ console.log(allBtn);
 // });
 // for (const button  of allBtn) {
 //    button.addEventListener("click", function () {
-//         console.log(button.textContent);
+//          console.log(button.textContent);
 //    })
 // }
 // for (let index = 0; index < allBtn.length; index++) {
 //     allBtn[index].addEventListener("click", function (params) {
 //         console.log(this.textContent);
 //     })
-    allBtn.forEach(function(button) {
-        button.addEventListener("click",function() {
-            console.log(this.textContent);
+    allBtn.forEach((element)=>{
+        element.addEventListener("click",(e)=>{
+            let num =0;
+            for (let index = 0; index < 1000000000; index++) {
+                num+=1;
+                
+            }
+            console.log(e.currentTarget.textContent, num);
         })
-    });
+    })
+    let n = 0;
+    for (let index = 0; index < 100000000; index++) {
+        n+=index;
+        
+    }
+    console.log("value of outer var", n);
+    console.log("script ends", n);
